5c7290f4f08a279edba0032407ec3331f4e4cd20
RamSanjay08 <ramsanjaydev08@gmail.com>
2024-03-19 19:41:37 +0530

    blacck add

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className='w-full max-h-screen relative'>
         <Image src={ImagesHome} alt="home" className='w-full max-h-screen object-contain' />
         <div className='w-full h-full absolute top-0 bg-black/30 text-center flex flex-col justify-center items-center  text-green-400 '>
-            <h2 className='text-[150px] font-bold'>..Blog Web..</h2>
+            <h2 className='text-[150px] font-bold bg-black'>..Blog Web..</h2>
             <p className='text-2xl font-bold translate-y-2 text-gray-100'>Explore the Innovative Blogs</p>
         </div>
     </div>
